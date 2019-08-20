598e01fdd37303017aac09ca944344e5c82ad7f1
Fernando <cuzcator@gmail.com>
2019-08-20 18:08:34 -0500

    Modelo Cliente
    Creamos el modelo para cliente, con las diferentes anotaciones para jpa.

---
+++ src/main/java/com/sistemas/springboot/app/models/entity/Cliente.java
@@ -0,0 +1,79 @@
+package com.sistemas.springboot.app.models.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity																			//persistencia
+@Table(name="clientes") 														//permite colocar un nombre sobre la base de datos
+public class Cliente implements Serializable{
+
+	@Id																			//inidica que el atributo id es la llave primaria sobre la DB.
+	@GeneratedValue(strategy = GenerationType.IDENTITY )						//Indica que el id sera autoincrementable
+	private Long id;
+
+	
+	private String nombre;
+	private String apellido;
+	private String email;
+	
+	@Column(name = "create_at")													//Indica el nombre del campo sobre la DB.
+	@Temporal(TemporalType.DATE)												//Indica el formato de la fecha
+	private Date createAt;														
+	
+	//es recomendable serializar la clase
+	private static final long serialVersionUID = 8014868644097168934L;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Date getCreateAt() {
+		return createAt;
+	}
+
+	public void setCreateAt(Date createAt) {
+		this.createAt = createAt;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+	
+}
