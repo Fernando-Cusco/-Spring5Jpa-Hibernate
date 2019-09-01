11ae34c903d297bdbeeff5f8deaacf109c33e9c3
Fernando <cuzcator@gmail.com>
2019-08-31 20:42:07 -0500

    Paginación
    import org.springframework.data.domain.Page;
    import org.springframework.data.domain.PageRequest;
    import org.springframework.data.domain.Pageable;

--- src/main/java/com/sistemas/springboot/app/models/dao/IClienteDao.java
+++ src/main/java/com/sistemas/springboot/app/models/dao/IClienteDao.java
@@ -1,9 +1,9 @@
 package com.sistemas.springboot.app.models.dao;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 import com.sistemas.springboot.app.models.entity.Cliente;
 
-public interface IClienteDao  extends CrudRepository<Cliente, Long>{    //CrudRepository
+public interface IClienteDao  extends PagingAndSortingRepository<Cliente, Long>{    //CrudRepository
 	
 }
--- src/main/java/com/sistemas/springboot/app/models/service/IClienteService.java
+++ src/main/java/com/sistemas/springboot/app/models/service/IClienteService.java
@@ -2,11 +2,16 @@ package com.sistemas.springboot.app.models.service;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 import com.sistemas.springboot.app.models.entity.Cliente;
 
 public interface IClienteService {
 
-public List<Cliente> findAll();
+	public List<Cliente> findAll();
+	
+	public Page<Cliente> findAll(Pageable pageable);
 	
 	public void save(Cliente cliente);
 	
